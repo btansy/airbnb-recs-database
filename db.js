01fa21e7675152fa5fe19d20678cd67510dfd091
Brian Tansy <btansy@gmail.com>
2020-02-27 14:18:51 -0800

    tested

--- db.js
+++ db.js
@@ -4,21 +4,42 @@ var db = {};
 
 db.pageload = function(primary_list_id, user_id, callback) {
     var pool = new Pool({
-      user: 'postgres',
-      host: 'localhost',
-      database: 'sdc',
-      password: 'postgres',
+      user: 'ubuntu',
+      host: '13.52.162.76',
+      database: 'ubuntu',
+      password: 'ubuntu',
       port: 5432,
     })
     pool.query(`SELECT * FROM recommendations, listings, opinions, images WHERE recommendations.primary_list_id = listings.id AND opinions.listing_id = listings.id AND images.list_id = listings.id AND recommendations.primary_list_id = ${primary_list_id} LIMIT 10`, (err, res) => {
       if (err) {
         callback(err, null);
+        console.log(err);
         pool.end();
       } else {
         callback(null, res);
         pool.end();
       }
     })
+}
+
+db.createRec = function(primary_list_id, secondary_list_id, callback) {
+  var pool = new Pool({
+    user: 'ubuntu',
+    host: '13.52.162.76',
+    database: 'ubuntu',
+    password: 'ubuntu',
+    port: 5432,
+  })
+  pool.query(`INSERT INTO recommendations (primary_list_id, secondary_list_id) VALUES (${primary_list_id},${secondary_list_id}) ON CONFLICT DO NOTHING`, (err, res) => {
+    if (err) {
+      callback(err, null);
+      console.log(err);
+      pool.end();
+    } else {
+      callback(null, res);
+      pool.end();
+    }
+  })
 }
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const newrelic = require('newrelic');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3003;
 app.use(express.json());
@@ -17,6 +18,19 @@ app.get('/pageload', (req, res) => {
     if (err) {
       res.status(400).send(err);
     } else {
+      res.status(200).send(data.rows);
+    }
+  })
+});
+
+app.put('/recommendations', (req, res) => {
+  console.log('POSTGRES PUT RECIEVED');
+  db.createRec( req.query.primary, req.query.secondary, (err, data) => {
+    if (err) {
+      console.log('POSTGRES PUT FAIL');
+      res.status(400).send(err);
+    } else {
+      console.log('POSTGRES PUT SUCCESS');
       res.status(200).send(data.rows);
     }
   })
